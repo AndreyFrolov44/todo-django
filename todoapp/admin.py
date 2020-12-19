1549627dd9a3696d61c3d8c31dd8eda8f96b119a
andre.frolov44@gmail.com <andre.frolov44@gmail.com>
2020-12-19 16:15:22 +0300

    Add block and item model

--- todoapp/admin.py
+++ todoapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, TodoBlock, TodoItem
 
 
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ('username', 'email', 'is_staff', 'is_active',)
@@ -21,4 +22,12 @@ class CustomUserAdmin(UserAdmin):
     ordering = ('email',)
 
 
-admin.site.register(CustomUser, CustomUserAdmin)
+@admin.register(TodoBlock)
+class TodoBlockAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'date_add',)
+
+
+@admin.register(TodoItem)
+class TodoItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'block', 'date_add',)
+
--- todoapp/models.py
+++ todoapp/models.py
@@ -26,3 +26,30 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.username
+
+
+class TodoBlock(models.Model):
+    name = models.CharField(max_length=200)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+    date_add = models.DateTimeField(default=timezone.now)
+    date_close = models.DateTimeField(default=timezone.now, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class TodoItem(models.Model):
+    name = models.CharField(max_length=250)
+    block = models.ForeignKey(TodoBlock, on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+    date_add = models.DateTimeField(default=timezone.now)
+    date_close = models.DateTimeField(default=timezone.now, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
